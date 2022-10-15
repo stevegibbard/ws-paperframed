befa9ce1f51bbed7d90dc92d5cafda20402a57bd
Steve Gibbard <33895548+stevegibbard@users.noreply.github.com>
2022-10-15 19:04:18 +0100

    Gatsby Tutorial Part 5

--- paperframed/src/pages/blog.js
+++ paperframed/src/pages/blog.js
@@ -9,10 +9,12 @@ const BlogPage = ({data}) => {
       <p>Latest Posts</p>
       <ul>
         {
-          data.allFile.nodes.map(node => (
-            <li key={node.name}>
-              {node.name}
-            </li>
+          data.allMdx.nodes.map(node => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <p>{node.excerpt}</p>
+            </article>
           ))
         }
       </ul>
@@ -22,9 +24,14 @@ const BlogPage = ({data}) => {
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt(pruneLength: 80)
       }
     }
   }
